918c261e44a6347c12e6f1dd07ca660a8736c311
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2018-12-13 11:25:40 +0100

    Added css section home-who

--- wordpress/wp-content/themes/cev/index.php
+++ wordpress/wp-content/themes/cev/index.php
@@ -10,8 +10,34 @@ Template Name: Page d’accueil
   <h1 class="hidden" aria-level="1" role="heading"><?php the_title(); ?></h1>
   <?php get_header('home'); ?>
   <?php include('navigation.php'); ?>
-  <section class="home__artistes wrap">
-      <h2 class="home__artistes-title title-red" aria-level="2" role="heading"><?= __('Qui sommes-nous ?','wp'); ?></h2>
+  <section class="home-who wrap">
+      <h2 class="home-who__title--icon title title__blue" aria-level="2" role="heading"><?= __('Qui sommes-nous ?','wp'); ?></h2>
+      <div class="home-who__container">
+          <div class="home-who__bloc">
+              <div class="home-who__bloc-text">
+                  <p>
+                      La CiTé – école vivante est un projet collaboratif de création d’un établissement
+                      secondaire différent. Il est porté par des enseignants chevronnés, désireux de soutenir
+                      le bien-être et l’exigence dans l’éducation des adolescents d’aujourd’hui, adultes de demain ;
+                      déjà citoyens.
+                  </p>
+                  <p>
+                      La CiTé – école vivante, s’inspire notamment du fonctionnement organisationnel et pédagogique
+                      du Collège-Lycée Expérimental (CLE) de Hérouville-Saint-Clair (Académie de Caen) en Normandie,
+                      inauguré en 1982. Comme cet établissement, La CiTé – école vivante ne se revendique d’aucun courant
+                      pédagogique, mais il fonde son action sur une principe simple : « Tout est éducatif ». Chaque élève y
+                      est considéré comme une personne, chaque adulte contribue à l’éducation et chaque dispositif soutient
+                      une intention.
+                  </p>
+              </div>
+              <a href="#" class="button-yellow"><?= __('En savoir plus','wp'); ?></a>
+          </div>
+          <div class="home-who__bloc-img">
+              <figure>
+                  <img src="<?= $images . '/../../images/blocImage.png';?>" alt="">
+              </figure>
+          </div>
+      </div>
   </section>
 
 	<?php include('footer.php'); ?>
