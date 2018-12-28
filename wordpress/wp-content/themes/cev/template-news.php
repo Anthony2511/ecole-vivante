7c92037c09a647a5ed11cec3ce14b3b2435b7269
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2018-12-28 10:26:32 +0100

    hover animation bloc home-news

--- wordpress/wp-content/themes/cev/template-news.php
+++ wordpress/wp-content/themes/cev/template-news.php
@@ -10,8 +10,8 @@ Template Name: Page News
 <h1 class="hidden" aria-level="1" role="heading"><?php the_title(); ?></h1>
     <?php include('navigation.php'); ?>
     <?php get_header(); ?>
-    <section class="wrap news-page">
-        <h2 class="" aria-level="2" role="heading"><?= __('Nos actualités/évènements','wp'); ?></h2>
+    <section class="news-page wrap">
+        <h2 class="title title__blue title-center" aria-level="2" role="heading"><?= __('Nos actualités/évènements','wp'); ?></h2>
     </section>
 
 <?php include('footer.php'); ?>
