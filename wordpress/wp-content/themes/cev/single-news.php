979de39601bcad41e6cbe737b4fbb2425058f486
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2018-12-28 14:53:26 +0100

    Added breadcrumb

--- wordpress/wp-content/themes/cev/single-news.php
+++ wordpress/wp-content/themes/cev/single-news.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Template Name: Page Single News
-*/
-; ?>
+*/; ?>
 
-<?php include('head.php'); ?>
+<?php include( 'head.php' ); ?>
 
 <body>
 <h1 class="hidden" aria-level="1" role="heading"><?php the_title(); ?></h1>
 <?php get_header(); ?>
-<?php include('navigation.php'); ?>
-<section class="wrap">
-	<h2 class="title title-red" aria-level="2" role="heading"><?php the_title(); ?></h2>
+<?php include( 'navigation.php' ); ?>
+<section class="single-news wrap">
+    <a href="<?php the_permalink(16); ?>" class="breadcrumb"><?= __('Retourner aux actualités','wp'); ?></a>
+    <h2 class="title title__blue title-center" aria-level="2" role="heading"><?php the_title(); ?></h2>
 </section>
