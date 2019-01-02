9afb7b894d60efcc1e6c06604811aa5748144ab3
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-02 15:24:00 +0100

    Added css page contact

--- wordpress/wp-content/themes/cev/template-contact.php
+++ wordpress/wp-content/themes/cev/template-contact.php
@@ -13,13 +13,13 @@ Template Name: Page Contact
         role="heading"><?= __( 'Contact', 'wp' ); ?></h2>
     <div class="contact-page__container">
         <div class="contact-page__bloc">
-            <figure>
+            <figure class="contact-page__logo">
                 <img src="<?= $images . '/../../images/LogoSiteNom.png'; ?>" alt="">
             </figure>
             <ul class="contact-page__list">
-                <li>Rue des charrons n°116</li>
-                <li>4800 Verviers</li>
-                <li>Belgique</li>
+                <li class="contact-page__item">Rue des charrons n°116</li>
+                <li class="contact-page__item">4800 Verviers</li>
+                <li class="contact-page__item">Belgique</li>
             </ul>
             <ul class="contact-page__list">
                 <li class="contact-page__item contact-page__item--tel">0478 45 85 96</li>
@@ -28,27 +28,28 @@ Template Name: Page Contact
         </div>
         <div class="contact-page__bloc">
             <form action="" class="contact-page__form">
-                <div class="contact-page__form__champ">
+                <legend class="contact-page__form-required">Les champs suivi d'un (*) sont obligatoires</legend>
+                <div class="contact-page__form-champ">
                     <label for="firstname" class="contact-page__form-label">Votre prénom</label>
                     <input type="text" id="firstname" class="contact-page__form-input" placeholder="Jean">
                 </div>
-                <div class="contact-page__form__champ">
+                <div class="contact-page__form-champ">
                     <label for="lastname" class="contact-page__form-label">Votre nom</label>
                     <input type="text" id="lastname" class="contact-page__form-input" placeholder="Dupont">
                 </div>
-                <div class="contact-page__form__champ">
+                <div class="contact-page__form-champ">
                     <label for="email" class="contact-page__form-label">Votre e-mail</label>
                     <input type="email" id="email" class="contact-page__form-input" placeholder="jean.dupont@gmail.com">
                 </div>
-                <div class="contact-page__form__champ">
+                <div class="contact-page__form-champ">
                     <label for="tel" class="contact-page__form-label">Votre téléphone</label>
                     <input type="number" id="tel" class="contact-page__form-input" placeholder="0478 98 85 72">
                 </div>
-                <div class="contact-page__form__champ">
+                <div class="contact-page__form-champ">
                     <label for="message" class="contact-page__form-label">Votre message</label>
-                    <textarea name="message" id="message" cols="30" rows="10" class="contact-page__form-textarea"></textarea>
+                    <textarea name="message" id="message" cols="30" rows="10" class="contact-page__form-input textarea" placeholder="Écrivez votre message..."></textarea>
                 </div>
-                <input type="submit" value="Envoyez votre message" class="button">
+                <input type="submit" value="Envoyez votre message" class="button-yellow">
             </form>
         </div>
     </div>
