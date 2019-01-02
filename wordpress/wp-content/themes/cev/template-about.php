efdcfcd01eb0db56e3075e6c7965ab5fcc4620d1
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-02 11:44:41 +0100

    Add section parcours page about

--- wordpress/wp-content/themes/cev/template-about.php
+++ wordpress/wp-content/themes/cev/template-about.php
@@ -24,8 +24,25 @@ Template Name: Page About
 				<?php $aboutImg = get_field('about-img'); ?>
 				<img src="<?= $aboutImg['url']; ?>" alt="<?= $aboutImg['alt']; ?>">
 			</figure>
+			<div class="who-page__parcours">
+				<h3 aria-level="3" role="heading" class="title-center title__blue"><?= __('Notre parcours','wp'); ?></h3>
+				<ul class="who-page__list">
+					<li class="who-page__item"><span>2016-2017</span>constitution d'un groupe de réflexion et définition des axes du projets </li>
+					<li class="who-page__item"><span>2017-06-27</span>lancement de la page Facebook de la CiTé.</li>
+					<li class="who-page__item"><span>2017-09-21</span>l'UAFA de l'ULiège rejoint l'équipe de la CiTé ! Bienvenue !</li>
+					<li class="who-page__item"><span>2017-10-03</span>première rencontre citoyenne "L'Apéro de la CiTé" où est notamment dévoilé notre nouveau logo</li>
+				</ul>
+			</div>
 		</div>
 </section>
+<div class="infos infos__white">
+	<div class="infos__bg"></div>
+	<section class="wrap">
+		<h2 class="infos__title" aria-level="2"
+		    role="heading"><?= __( 'Besoin de nous contacter ?', 'wp' ); ?></h2>
+		<a href="<?php the_permalink(); ?>" class="button-yellow button-center"><?= __( 'Contactez-nous', 'wp' ); ?></a>
+	</section>
+</div>
 
 
 <?php include( 'footer.php' ); ?>
