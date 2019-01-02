59899d15d24baa3261642ac4700fab7b763b8cff
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-02 11:21:37 +0100

    Added dynamic content about page to home page

--- wordpress/wp-content/themes/cev/index.php
+++ wordpress/wp-content/themes/cev/index.php
@@ -14,32 +14,24 @@ include( 'head.php' ); ?>
     <h2 class="home-who__title--icon title title__blue" aria-level="2"
         role="heading"><?= __( 'Qui sommes-nous ?', 'wp' ); ?></h2>
     <div class="home-who__container">
+	    <?php $posts = new WP_Query( [
+		    'page_id' => 42,
+	    ] ); ?>
+	    <?php if ( $posts->have_posts() ) : while ( $posts->have_posts() ): $posts->the_post(); ?>
         <div class="home-who__bloc">
             <div class="home-who__bloc-text">
-                <p>
-                    La CiTé – école vivante est un projet collaboratif de création d’un établissement
-                    secondaire différent. Il est porté par des enseignants chevronnés, désireux de soutenir
-                    le bien-être et l’exigence dans l’éducation des adolescents d’aujourd’hui, adultes de demain ;
-                    déjà citoyens.
-                </p>
-                <p>
-                    La CiTé – école vivante, s’inspire notamment du fonctionnement organisationnel et pédagogique
-                    du Collège-Lycée Expérimental (CLE) de Hérouville-Saint-Clair (Académie de Caen) en Normandie,
-                    inauguré en 1982. Comme cet établissement, La CiTé – école vivante ne se revendique d’aucun courant
-                    pédagogique, mais il fonde son action sur une principe simple : « Tout est éducatif ». Chaque élève
-                    y
-                    est considéré comme une personne, chaque adulte contribue à l’éducation et chaque dispositif
-                    soutient
-                    une intention.
-                </p>
+                <?= wp_trim_words( get_field( 'about-intro-text' ), 114, '...' ); ?>
             </div>
             <a href="<?php the_permalink('42'); ?>" class="button-yellow"><?= __( 'En savoir plus', 'wp' ); ?></a>
         </div>
         <div class="home-who__bloc-img">
             <figure>
-                <img src="<?= $images . '/../../images/imageAbout.png'; ?>" alt="">
+		        <?php $aboutImg = get_field('about-img'); ?>
+                <img src="<?= $aboutImg['url']; ?>" alt="<?= $aboutImg['alt']; ?>">
             </figure>
         </div>
+		    <?php wp_reset_postdata(); ?>
+	    <?php endwhile; endif; ?>
     </div>
 </section>
 <div class="home-news__background">
