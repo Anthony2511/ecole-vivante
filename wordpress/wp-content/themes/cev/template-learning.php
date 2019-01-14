9511c5c00e6dc5dc1123c82c57a3100a8775e70c
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-14 11:37:34 +0100

    Added structure & css page learning

--- wordpress/wp-content/themes/cev/template-learning.php
+++ wordpress/wp-content/themes/cev/template-learning.php
@@ -12,6 +12,116 @@ Template Name: Page Learning
 <section class="members-page wrap">
     <h2 class="title-center title__blue" aria-level="2"
         role="heading"><?= __( 'Le temps d\'apprentissage', 'wp' ); ?></h2>
+    <div class="learning-page__container">
+        <div class="learning-page__bloc">
+            <section class="learning-page__infos">
+                <h3 class="learning-page__title" aria-level="3" role="heading">Temps de décloisonnement disciplinaire</h3>
+                <p class="learning-page__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam tincidunt et neque ultrices cursus. Mauris a ullamcorper
+                    nibh, in hendrerit metus. Proin ultrices, est ac maximus dictum,
+                    elit sem ultrices sem, in molestie quam lorem ut felis. Vestibulum
+                    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    Curae; Pellentesque vehicula venenatis nulla, ut sodales metus vestibulum
+                    tincidunt. Donec nec nunc tellus. Nullam placerat fermentum felis mattis
+                    elementum.
+                </p>
+            </section>
+            <figure class="learning-page__figure">
+                <img src="<?= $images . '/../../images/learning1.png'; ?>" alt="">
+            </figure>
+        </div>
+        <div class="learning-page__bloc">
+            <section class="learning-page__infos">
+                <h3 class="learning-page__title" aria-level="3" role="heading">Atelier disciplinaire</h3>
+                <p class="learning-page__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam tincidunt et neque ultrices cursus. Mauris a ullamcorper
+                    nibh, in hendrerit metus. Proin ultrices, est ac maximus dictum,
+                    elit sem ultrices sem, in molestie quam lorem ut felis. Vestibulum
+                    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    Curae; Pellentesque vehicula venenatis nulla, ut sodales metus vestibulum
+                    tincidunt. Donec nec nunc tellus. Nullam placerat fermentum felis mattis
+                    elementum.
+                </p>
+            </section>
+            <figure class="learning-page__figure">
+                <img src="<?= $images . '/../../images/learning2.png'; ?>" alt="">
+            </figure>
+        </div>
+        <div class="learning-page__bloc">
+            <section class="learning-page__infos">
+                <h3 class="learning-page__title" aria-level="3" role="heading">Enseignement massé</h3>
+                <p class="learning-page__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam tincidunt et neque ultrices cursus. Mauris a ullamcorper
+                    nibh, in hendrerit metus. Proin ultrices, est ac maximus dictum,
+                    elit sem ultrices sem, in molestie quam lorem ut felis. Vestibulum
+                    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    Curae; Pellentesque vehicula venenatis nulla, ut sodales metus vestibulum
+                    tincidunt. Donec nec nunc tellus. Nullam placerat fermentum felis mattis
+                    elementum.
+                </p>
+            </section>
+            <figure class="learning-page__figure">
+                <img src="<?= $images . '/../../images/learning3.png'; ?>" alt="">
+            </figure>
+        </div>
+        <div class="learning-page__bloc">
+            <section class="learning-page__infos">
+                <h3 class="learning-page__title" aria-level="3" role="heading">Disciplines ressources</h3>
+                <p class="learning-page__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam tincidunt et neque ultrices cursus. Mauris a ullamcorper
+                    nibh, in hendrerit metus. Proin ultrices, est ac maximus dictum,
+                    elit sem ultrices sem, in molestie quam lorem ut felis. Vestibulum
+                    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    Curae; Pellentesque vehicula venenatis nulla, ut sodales metus vestibulum
+                    tincidunt. Donec nec nunc tellus. Nullam placerat fermentum felis mattis
+                    elementum.
+                </p>
+            </section>
+            <figure class="learning-page__figure">
+                <img src="<?= $images . '/../../images/learning4.png'; ?>" alt="">
+            </figure>
+        </div>
+        <div class="learning-page__bloc">
+            <section class="learning-page__infos">
+                <h3 class="learning-page__title" aria-level="3" role="heading">Moi,apprenant</h3>
+                <p class="learning-page__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam tincidunt et neque ultrices cursus. Mauris a ullamcorper
+                    nibh, in hendrerit metus. Proin ultrices, est ac maximus dictum,
+                    elit sem ultrices sem, in molestie quam lorem ut felis. Vestibulum
+                    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    Curae; Pellentesque vehicula venenatis nulla, ut sodales metus vestibulum
+                    tincidunt. Donec nec nunc tellus. Nullam placerat fermentum felis mattis
+                    elementum.
+                </p>
+            </section>
+            <figure class="learning-page__figure">
+                <img src="<?= $images . '/../../images/learning5.png'; ?>" alt="">
+            </figure>
+        </div>
+        <div class="learning-page__bloc">
+            <section class="learning-page__infos">
+                <h3 class="learning-page__title" aria-level="3" role="heading">Référant PIA</h3>
+                <p class="learning-page__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam tincidunt et neque ultrices cursus. Mauris a ullamcorper
+                    nibh, in hendrerit metus. Proin ultrices, est ac maximus dictum,
+                    elit sem ultrices sem, in molestie quam lorem ut felis. Vestibulum
+                    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    Curae; Pellentesque vehicula venenatis nulla, ut sodales metus vestibulum
+                    tincidunt. Donec nec nunc tellus. Nullam placerat fermentum felis mattis
+                    elementum.
+                </p>
+            </section>
+            <figure class="learning-page__figure">
+                <img src="<?= $images . '/../../images/learning6.png'; ?>" alt="">
+            </figure>
+        </div>
+    </div>
 </section>
 <div class="infos infos__white">
     <div class="infos__bg"></div>
