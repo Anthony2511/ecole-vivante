56452bc23a68a6501c7b9aab67f9f52cd73feb35
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-19 16:41:04 +0100

    Stylized filter news

--- wordpress/wp-content/themes/cev/parts/switcher-news.php
+++ wordpress/wp-content/themes/cev/parts/switcher-news.php
@@ -6,7 +6,7 @@ $terms = get_terms('category', [
 ]);
 ?>
 <form method="GET" class="sorting__form hide" id="filter-news">
-	<label for="post-filter-news" class="sorting__label">Filtrer les actualités</label>
+	<label for="post-filter-news" class="sorting__label"><?= __('Filtrer les actualités','wp'); ?></label>
 	<select id="post-filter-news" name="post-filter-news" class="sorting__select">
 		<option value="none" class="sorting__option">Tous</option>
 		<?php foreach($terms as $term): ?>
--- wordpress/wp-content/themes/cev/template-news.php
+++ wordpress/wp-content/themes/cev/template-news.php
@@ -12,7 +12,7 @@ Template Name: Page News
 <section class="home-news wrap news-page">
     <h2 class="home-news__title home-news__title--icon title-center title__blue" aria-level="2"
         role="heading"><?= __( 'Nos dernières actualités/évènements', 'wp' ); ?></h2>
-    <?php include('parts/switcher-news.php'); ?>
+	<?php include( 'parts/switcher-news.php' ); ?>
     <div class="home-news__container">
 		<?php $posts = new WP_Query( [
 			'posts_per_page' => - 1,
@@ -21,7 +21,8 @@ Template Name: Page News
 			'order'          => 'DESC'
 		] ); ?>
 		<?php if ( $posts->have_posts() ) : while ( $posts->have_posts() ): $posts->the_post(); ?>
-            <div class="home-news__bloc news-page__bloc <?= wp_display_taxonomy_terms($post->ID, 'category', 'slug'); ?>" id="post-<?php the_ID(); ?>">
+            <div class="home-news__bloc news-page__bloc <?= wp_display_taxonomy_terms( $post->ID, 'category', 'slug' ); ?>"
+                 id="post-<?php the_ID(); ?>">
 				<?php $homeNews = get_field( 'news-img' ); ?>
                 <figure class="home-news__figure">
 					<?php if ( ! empty( $homeNews ) ): ?>
@@ -38,13 +39,16 @@ Template Name: Page News
 					<?php endif; ?>
 
                     <h3 class="home-news__bloc-title"><?= the_title(); ?></h3>
-                    <time class="home-news__bloc-date" datetime="<?= get_the_date("d/m/y"); ?>"><?= get_the_date(); ?></time>
+                    <time class="home-news__bloc-date"
+                          datetime="<?= get_the_date( "d/m/y" ); ?>"><?= get_the_date(); ?></time>
                     <p class="home-news__bloc-text">
 						<?= wp_trim_words( get_field( 'news-text' ), 29, '...' ); ?>
                     </p>
-                    <a href="<?= the_permalink(); ?>" class="button-white" title="Vers l'article : <?= the_title(); ?>"><?= __( 'En savoir plus', 'wp' ); ?></a>
+                    <a href="<?= the_permalink(); ?>" class="button-white"
+                       title="Vers l'article : <?= the_title(); ?>"><?= __( 'En savoir plus', 'wp' ); ?></a>
                 </section>
-                <a href="<?= the_permalink(); ?>" class="home-news__bloc-link" title="Vers l'article : <?= the_title(); ?>"></a>
+                <a href="<?= the_permalink(); ?>" class="home-news__bloc-link"
+                   title="Vers l'article : <?= the_title(); ?>"></a>
             </div>
 			<?php wp_reset_postdata(); ?>
 		<?php endwhile; endif; ?>
@@ -55,7 +59,8 @@ Template Name: Page News
     <section class="wrap">
         <h2 class="infos__title" aria-level="2"
             role="heading"><?= __( 'Besoin d\'informations sur L\'école vivante ?', 'wp' ); ?></h2>
-        <a href="<?php the_permalink('42'); ?>" class="button-yellow button-center"><?= __( 'En savoir plus', 'wp' ); ?></a>
+        <a href="<?php the_permalink( '42' ); ?>"
+           class="button-yellow button-center"><?= __( 'En savoir plus', 'wp' ); ?></a>
     </section>
 </div>
 
