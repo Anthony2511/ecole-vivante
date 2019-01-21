6664223dbeb1a9d3236a9e80c783ffb2b4a7cfdb
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-21 12:25:14 +0100

    Added loop page time

--- wordpress/wp-content/themes/cev/template-time.php
+++ wordpress/wp-content/themes/cev/template-time.php
@@ -5,25 +5,35 @@ Template Name: Page Time
 
 <?php include( 'head.php' ); ?>
 
-	<body>
+    <body>
 <h1 class="hidden" aria-level="1" role="heading"><?php the_title(); ?></h1>
 <?php include( 'navigation.php' ); ?>
 <?php get_header(); ?>
 <section class="time-page wrap">
-	<h2 class="title-center title__blue" aria-level="2"
-	    role="heading"><?= __( 'Nos horaires', 'wp' ); ?></h2>
-	<div class="time-page__container">
+    <h2 class="title-center title__blue" aria-level="2"
+        role="heading"><?= __( 'Nos horaires', 'wp' ); ?></h2>
+    <div class="time-page__container">
+		<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+            <div class="time-page__bloc">
+				<?php if ( have_rows( 'horaire' ) ): ?>
+					<?php while ( have_rows( 'horaire' ) ): the_row(); ?>
+                        <a href="<?= the_sub_field( 'time-pdf' ); ?>" target="_blank" class="time-page__link">
+                            <?= the_sub_field('time-name'); ?>
+                        </a>
+					<?php endwhile; endif; ?>
+            </div>
+		<?php endwhile; endif; ?>
 
-	</div>
+    </div>
 </section>
 <div class="infos infos__white">
-	<div class="infos__bg"></div>
-	<section class="wrap">
-		<h2 class="infos__title" aria-level="2"
-		    role="heading"><?= __( 'Envie d\' en savoir plus ?', 'wp' ); ?></h2>
-		<a href="<?php the_permalink('42'); ?>"
-		   class="button-yellow button-center"><?= __( 'En savoir plus', 'wp' ); ?></a>
-	</section>
+    <div class="infos__bg"></div>
+    <section class="wrap">
+        <h2 class="infos__title" aria-level="2"
+            role="heading"><?= __( 'Envie d\' en savoir plus ?', 'wp' ); ?></h2>
+        <a href="<?php the_permalink( '42' ); ?>"
+           class="button-yellow button-center"><?= __( 'En savoir plus', 'wp' ); ?></a>
+    </section>
 </div>
 
 <?php include( 'footer.php' ); ?>
